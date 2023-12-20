cc1772f733d36150a4b057819f5ef7c5ee5c71cc
sergeygurvich <segurvich@ucsd.edu>
2023-12-19 17:48:13 -0800

    Keycloak MLFlow Integration - Attempt 1

--- core/run.sh
+++ core/run.sh
@@ -6,4 +6,5 @@ mlflow server \
     --host 0.0.0.0 \
     --port ${MLFLOW_PORT} \
     --backend-store-uri ${MLFLOW_FILE_STORE} \
-    --default-artifact-root ${MLFLOW_ARTIFACT_STORE}
+    --default-artifact-root ${MLFLOW_ARTIFACT_STORE} \
+    --app-name ndp_mlflow_auth
